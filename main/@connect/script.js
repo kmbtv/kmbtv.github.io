6ff5acb7f2d050db61d3d1bd833bd83f703ec1de
kmbtv <kmbytpmv@gmail.com>
2024-08-27 16:04:40 -0400

    Update script.js

--- main/@connect/script.js
+++ main/@connect/script.js
@@ -36043,7 +36043,7 @@ from your past games list.`,
     kae = {
         CAMERA: "[b]HEADS UP:[/b] We’re not detecting a camera, but you can still play the game without a photo. If this seems wrong, try joining with a different browser.",
         STYLE: "[b]HEADS UP:[/b] Your browser seems a bit outdated, and will have some issues displaying this game.",
-        TOS: "By clicking {submit}, you agree to our [tos]Terms of Service[/tos]"
+        TOS: "KMBTV is not affiliated with Kill Me Baby nor Jackbox Games. \n Jackbox [tos]Terms of Service[/tos] still apply."
     },
     Gae = {
         BRANCH: wae,
